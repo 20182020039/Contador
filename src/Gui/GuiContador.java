9c22cdf093f329765de3884d3a1e9b2fba3c048e
20182020039 <sebastianmt512@gmail.com>
2019-05-15 11:08:08 -0500

    Edicion

--- src/Gui/GuiContador.java
+++ src/Gui/GuiContador.java
@@ -5,17 +5,20 @@
  */
 package Gui;
 
+import Logica.Contador;
+
 /**
  *
  * @author Estudiantes
  */
 public class GuiContador extends javax.swing.JFrame {
-
+private Contador contador = new Contador();
     /**
      * Creates new form GuiContador
      */
     public GuiContador() {
         initComponents();
+        LblValor.setText(String.valueOf(contador.getValor()));
     }
 
     /**
@@ -30,6 +33,9 @@ public class GuiContador extends javax.swing.JFrame {
         LblEtiquetaValor = new javax.swing.JLabel();
         LblTitulo = new javax.swing.JLabel();
         LblValor = new javax.swing.JLabel();
+        BtnContar = new javax.swing.JButton();
+        BtnDescontar = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,17 +51,48 @@ public class GuiContador extends javax.swing.JFrame {
         LblValor.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         LblValor.setText("0");
 
+        BtnContar.setFont(new java.awt.Font("SWTxt", 1, 14)); // NOI18N
+        BtnContar.setText("Contar");
+        BtnContar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnContarActionPerformed(evt);
+            }
+        });
+
+        BtnDescontar.setFont(new java.awt.Font("SWTxt", 1, 14)); // NOI18N
+        BtnDescontar.setText("Descontar");
+        BtnDescontar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnDescontarActionPerformed(evt);
+            }
+        });
+
+        jButton3.setFont(new java.awt.Font("SWTxt", 1, 14)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(153, 0, 0));
+        jButton3.setText("Borrar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(LblTitulo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addGap(6, 6, 6)
-                .addComponent(LblEtiquetaValor, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(LblValor, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 31, Short.MAX_VALUE))
-            .addComponent(LblTitulo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton3)
+                    .addComponent(BtnDescontar)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(BtnContar)
+                            .addComponent(LblEtiquetaValor, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(LblValor, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 85, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -66,11 +103,32 @@ public class GuiContador extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(LblEtiquetaValor)
                     .addComponent(LblValor))
-                .addContainerGap(223, Short.MAX_VALUE))
+                .addGap(48, 48, 48)
+                .addComponent(BtnContar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(BtnDescontar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
+                .addContainerGap(82, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BtnContarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnContarActionPerformed
+        contador.Avanzar();
+        LblValor.setText(String.valueOf(contador.getValor()));
+    }//GEN-LAST:event_BtnContarActionPerformed
+
+    private void BtnDescontarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnDescontarActionPerformed
+       contador.Retroceder();
+        LblValor.setText(String.valueOf(contador.getValor()));
+    }//GEN-LAST:event_BtnDescontarActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        contador.Borrar();
+        LblValor.setText(String.valueOf(contador.getValor()));
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -108,8 +166,11 @@ public class GuiContador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnContar;
+    private javax.swing.JButton BtnDescontar;
     private javax.swing.JLabel LblEtiquetaValor;
     private javax.swing.JLabel LblTitulo;
     private javax.swing.JLabel LblValor;
+    private javax.swing.JButton jButton3;
     // End of variables declaration//GEN-END:variables
 }
